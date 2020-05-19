a9ab7804dcd71e0b9276f740d47a72d8ea4c86e8
Fedor <andryushin.fedor@gmail.com>
2020-05-19 17:17:10 +0300

    animation upd

--- js/sketchParty.js
+++ js/sketchParty.js
@@ -1,97 +1,78 @@
-let dots = [];
-let count;
-let cellSize = 60;
 
 function setup() {
   let myCanvas = createCanvas(windowWidth, windowHeight, WEBGL);
   myCanvas.parent("canvas");
-  
-  let xCellsCount = width / cellSize;
-  let yCellsCount = height / cellSize;
-  count = xCellsCount * yCellsCount;
 
-  for (let yCount = 0; yCount < yCellsCount; yCount++) {
-    for (let xCount = 0; xCount < xCellsCount; xCount++) {
-      dots.push(
-        createDot({
-          xCount,
-          yCount,
-          cellSize
-        })
-      )
-    }
-  }
 }
+let ball = {
+  x: 90,
+  y: 80,
+  xspeed: 1,
+  yspeed: -2,
+}
+let ball2 = {
+  x: 60,
+  y: 70,
+  xspeed: 2,
+  yspeed: 3,
+}
+
 
 function draw() {
-  background('FFCC48');
-  
-  push();
-  translate(- width / 2, - height / 2)
-  for (let i = 0; i < count; i++) {
-    dots[i].move();
-    dots[i].display();
-  }
-  pop();
-  
-  push();
-  
-  pop();
+  background('#68DBFF');
+  //   earth
+  fill('#C6FF68')
+  noStroke()
+  //   sun
+  fill('#FFD600')
+  ellipse(width - mouseX, 300, 300, 300)
+  //   cloud
+
+  //   ball-1
+  display()
+  move()
+  bounce()
+  //   ball-2
+  display2()
+  move2()
+  bounce2()
 }
 
-function createDot({
-  xCount,
-  yCount,
-  cellSize,
-  d = 12,
-  speed = random(0.04, 2)
-}) {
-  let paddingX = cellSize / 2;
-  let paddingY = cellSize / 2;
-  let velocity = speed * 1.2;
-  let directionX = 1;
-  let directionY = 1;
+function display() {
+  fill('#AD00FF')
+  ellipse(ball.x, ball.y, 135, 135)
+}
 
-  return {
-    move() {
-      paddingX = paddingX + speed * directionX;
-      if (paddingX >= cellSize || paddingX <= 0) {
-        directionX *= -1;
-        paddingX = paddingX + velocity * directionX;
-        paddingY = paddingY + velocity * directionY;
-      }
-      if (paddingY >= cellSize || paddingY <= 0) {
-        directionY *= -1;
-        paddingY = paddingY + velocity * directionY;
-      }
-    },
-    display() {
-      let x = xCount * cellSize + paddingX;
-      let y = yCount * cellSize + paddingY;
-      
-      fill(random(0, 255), random(0, 255), random(0, 255));
-      noStroke();
-      ellipse(x, y, d);
-      
-      if (mouseIsPressed) {
-        noFill();
-        stroke(55, 50);
-        rect(x - paddingX, y - paddingY, cellSize, cellSize);
-        push();
-        translate(- cellSize / 5, - cellSize / 5)
-        noFill();
-        stroke(55, 50);
-        rect(x, y, 2 * d, 2 * d);
-        pop();
-        
-        fill(55);
-        noStroke();
-        ellipse(x, y, d);
+function move() {
+  ball.x = ball.x + ball.xspeed
+  ball.y = ball.y - ball.yspeed
+}
 
-        // stroke(255, 50);
-        // line(mouseX, mouseY, x, y);
-      }
-    }
+function bounce() {
+  if (ball.x > width || ball.x < 0) {
+    ball.xspeed = ball.xspeed * -1;
+  }
+  if (ball.y > height || ball.y < 0) {
+    ball.yspeed = ball.yspeed * -1;
+  }
+}
+
+function display2() {
+  fill('#FED535')
+  ellipse(ball2.x, ball2.y, 85, 85)
+}
+
+function move2() {
+  ball2.x = ball2.x + ball2.xspeed
+  ball2.y = ball2.y - ball2.yspeed
+}
+
+function bounce2() {
+  if (ball2.x > width || ball2.x < 0) {
+    ball2.xspeed = ball2.xspeed * -1;
+  }
+  if (ball2.y > height || ball2.y < 0) {
+    ball2.yspeed = ball2.yspeed * -1;
   }
 }
 
