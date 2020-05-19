970db0a27aa930ff87a9f41ca18b7a10d386f663
Fedor <andryushin.fedor@gmail.com>
2020-05-19 11:26:33 +0300

    added 2 animation

--- js/sketchExs.js
+++ js/sketchExs.js
@@ -10,37 +10,54 @@ function setup() {
   myCanvas.parent("canvas");
   background('#FFCC48');
 }
+let circles = 14;
+let radius = 100;
+let numSpacing = radius * 0.25;
+let speed = 0.001;
 
 function draw() {
-  fill(202, 260, 202, 25);
-  noStroke();
-  rect(0, 0, windowWidth, windowHeight);
+  background('#FFCC48');
 
-  display()
-  move()
-  bounce()
-}
-
-function display() {
-  noStroke()
-  fill(255, 100, 20, random(0, 255));
-  ellipse(ball.x, ball.y, 150, 150);
-}
-
-function move() {
-  ball.x = ball.x + ball.xspeed
-  ball.y = ball.y + ball.yspeed
-}
-
-function bounce() {
-  if (ball.x > width || ball.x < 0) {
-    ball.xspeed = ball.xspeed * -1
-  }
-  if (ball.y > height || ball.y < 0) {
-    ball.yspeed = ball.yspeed * -1
+  for (let i = 0; i < circles; i++) {
+    push();
+    translate(width / 2, height / 2);
+    createCircle({
+      radius: radius + numSpacing * i,
+      numsAmount: radius / 2 + i,
+      seed: i
+    });
+    pop();
   }
 }
 
+function createCircle({
+  radius,
+  numsAmount,
+  seed
+}) {
+  randomSeed(seed);
+  let numAngle = 2 * PI / numsAmount;
+  let nums = [];
+  for (let i = 0; i < numsAmount; i++) {
+    nums.push(int(random(2)));
+  }
+  rotate(frameCount * numsAmount * speed);
+  push();
+  for (let i = 0; i < numsAmount; i++) {
+    push();
+    // rotate(frameCount * i * numAngle * 0.005);
+    // Двигаем число по кругу 
+    rotate(i * numAngle);
+    // Отодвигаем от центра на радиус
+    translate(0 + radius, 0);
+    // Поворачиваем каждое число на 90 градусов
+    rotate(PI / 2);
+    fill(255 / seed + seed);
+    text(nums[i], 0, 0);
+    pop();
+  }
+  pop();
+}
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
